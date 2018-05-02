8a1350bdbab707b2b48a8552ad8b5048eb318929
Vincent Chen <vincentschen@yahoo.com>
2018-05-01 23:42:15 -0700

    Fix _rebuild_tensor_v2 to work with model saved using different pytorch

--- lib/word_vectors.py
+++ lib/word_vectors.py
@@ -13,6 +13,20 @@ from tqdm import tqdm
 
 from config import DATA_PATH
 
+# Monkey-patch because I trained with a newer version.
+# This can be removed once PyTorch 0.4.x is out.
+# See https://discuss.pytorch.org/t/question-about-rebuild-tensor-v2/14560
+import torch._utils
+try:
+    torch._utils._rebuild_tensor_v2
+except AttributeError:
+    def _rebuild_tensor_v2(storage, storage_offset, size, stride, requires_grad, backward_hooks):
+        tensor = torch._utils._rebuild_tensor(storage, storage_offset, size, stride)
+        tensor.requires_grad = requires_grad
+        tensor._backward_hooks = backward_hooks
+        return tensor
+    torch._utils._rebuild_tensor_v2 = _rebuild_tensor_v2
+####
 
 def obj_edge_vectors(names, wv_type='glove.6B', wv_dir=DATA_PATH, wv_dim=300):
     wv_dict, wv_arr, wv_size = load_word_vectors(wv_dir, wv_type, wv_dim)
