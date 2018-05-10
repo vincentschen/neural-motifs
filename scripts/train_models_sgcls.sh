ea377f46bcaf3a74df479baafa2786e0aab5935b
Vincent Chen <vincentschen@yahoo.com>
2018-05-09 17:37:02 -0700

    update train scripts to work with current baseline/reef models

--- scripts/train_models_sgcls.sh
+++ scripts/train_models_sgcls.sh
@@ -4,22 +4,35 @@
 export CUDA_VISIBLE_DEVICES=$2
 
 if [ $1 == "0" ]; then
-    echo "TRAINING THE BASELINE"
-    python models/train_rels.py -m sgcls -model motifnet -nl_obj 0 -nl_edge 0 -b 6 \
-    -clip 5 -p 100 -pooling_dim 4096 -lr 1e-3 -ngpu 1 -ckpt checkpoints/vgdet/vg-24.tar -save_dir checkpoints/baseline-sgcls \
-    -nepoch 50 -use_bias
-elif [ $1 == "1" ]; then
-    echo "TRAINING MESSAGE PASSING"
+    echo "TRAINING BASELINE"
 
-    python models/train_rels.py -m sgcls -model stanford -b 6 -p 100 -lr 1e-3 -ngpu 1 -clip 5 \
-    -ckpt checkpoints/vgdet/vg-24.tar -save_dir checkpoints/stanford-sgcls
-elif [ $1 == "2" ]; then
+    BASELINE_CHECKPOINT=motifnet-sgcls-baserels
+    VG_SGG_FN='10R-baseline-VG-SGG.h5'
+    VG_SGG_DICT_FN='10R-baseline-VG-SGG-dicts.json'
+    #HACK: replace lines in the config file with dataset names
+    sed -i "s/VG_SGG_FN.*/VG_SGG_FN = stanford_path('$VG_SGG_FN')/" config.py
+    sed -i "s/VG_SGG_DICT_FN.*/VG_SGG_DICT_FN = stanford_path('$VG_SGG_DICT_FN')/" config.py
+    python models/train_rels.py \
+        -m sgcls -model motifnet -order leftright -nl_obj 2 -nl_edge 4 -b 6 -clip 5 \
+        -p 100 -hidden_dim 512 -pooling_dim 4096 -lr 1e-3 \
+        -ngpu 1 \
+        -ckpt checkpoints/vgdet/vg-24.tar \
+        -save_dir checkpoints/$BASELINE_CHECKPOINT -nepoch 50 -use_bias
+
+elif [ $1 == "1" ]; then
     echo "TRAINING MOTIFNET"
 
-    python models/train_rels.py -m sgcls -model motifnet -order leftright -nl_obj 2 -nl_edge 4 -b 6 -clip 5 \
-        -p 100 -hidden_dim 512 -pooling_dim 4096 -lr 1e-3 -ngpu 1 -ckpt checkpoints/vgdet/vg-24.tar \
-        -save_dir checkpoints/motifnet-sgcls-baserels -nepoch 50 -use_bias
+    REEF_CHECKPOINT=motifnet-sgcls-reefrels
+    VG_SGG_FN='10R-reef-VG-SGG.h5'
+    VG_SGG_DICT_FN='10R-reef-VG-SGG-dicts.json'
+    #HACK: replace lines in the config file with dataset names
+    sed -i "s/VG_SGG_FN.*/VG_SGG_FN = stanford_path('$VG_SGG_FN')/" config.py
+    sed -i "s/VG_SGG_DICT_FN.*/VG_SGG_DICT_FN = stanford_path('$VG_SGG_DICT_FN')/" config.py
+    python models/train_rels.py \
+        -m sgcls -model motifnet -order leftright -nl_obj 2 -nl_edge 4 -b 6 -clip 5 \
+        -p 100 -hidden_dim 512 -pooling_dim 4096 -lr 1e-3 \
+        -ngpu 1 \
+        -ckpt checkpoints/vgdet/vg-24.tar \
+        -save_dir checkpoints/$REEF_CHECKPOINT -nepoch 50 -use_bias
 fi
-
-
 
