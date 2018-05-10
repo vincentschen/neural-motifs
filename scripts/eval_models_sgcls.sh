7dd7214285d5252236925ed51524e4a9c35a8ac0
Vincent Chen <vincentschen@yahoo.com>
2018-05-10 13:56:55 -0700

    Add script for evaluation

--- scripts/eval_models_sgcls.sh
+++ scripts/eval_models_sgcls.sh
@@ -5,24 +5,22 @@ export CUDA_VISIBLE_DEVICES=$2
 
 if [ $1 == "0" ]; then
     echo "EVALING THE BASELINE"
-    python models/eval_rels.py -m sgcls -model motifnet -nl_obj 0 -nl_edge 0 -b 6 \
-    -clip 5 -p 100 -pooling_dim 4096 -lr 1e-3 -ngpu 1 -ckpt checkpoints/baseline-sgcls/vgrel-11.tar \
-    -nepoch 50 -use_bias -test -cache baseline_sgcls
-    python models/eval_rels.py -m predcls -model motifnet -nl_obj 0 -nl_edge 0 -b 6 \
-    -clip 5 -p 100 -pooling_dim 4096 -lr 1e-3 -ngpu 1 -ckpt checkpoints/baseline-sgcls/vgrel-11.tar \
-    -nepoch 50 -use_bias -test -cache baseline_predcls
+
+    BASELINE_CHECKPOINT=motifnet-sgcls-baserels
+    CHECKPOINT_FN=vgrel-12.tar
+    python models/eval_rels.py -m sgcls -model motifnet -order leftright -nl_obj 2 -nl_edge 4 -b 6 -clip 5 \
+        -p 100 -hidden_dim 512 -pooling_dim 4096 -lr 1e-3 -ngpu 1 -test -ckpt checkpoints/$BASELINE_CHECKPOINT/$CHECKPOINT_FN -nepoch 50 -use_bias
+    python models/eval_rels.py -m predcls -model motifnet -order leftright -nl_obj 2 -nl_edge 4 -b 6 -clip 5 \
+        -p 100 -hidden_dim 512 -pooling_dim 4096 -lr 1e-3 -ngpu 1 -test -ckpt checkpoints/$BASELINE_CHECKPOINT/$CHECKPOINT_FN -nepoch 50 -use_bias
 elif [ $1 == "1" ]; then
-    echo "EVALING MESSAGE PASSING"
-    python models/eval_rels.py -m sgcls -model stanford -b 6 -p 100 -lr 1e-3 -ngpu 1 -clip 5 \
-    -ckpt checkpoints/stanford-sgcls/vgrel-11.tar -test -cache stanford_sgcls
-    python models/eval_rels.py -m predcls -model stanford -b 6 -p 100 -lr 1e-3 -ngpu 1 -clip 5 \
-    -ckpt checkpoints/stanford-sgcls/vgrel-11.tar -test -cache stanford_predcls
-elif [ $1 == "2" ]; then
+
+    REEF_CHECKPOINT=motifnet-sgcls-reefrels
+    CHECKPOINT_FN=vgrel-12.tar
     echo "EVALING MOTIFNET"
     python models/eval_rels.py -m sgcls -model motifnet -order leftright -nl_obj 2 -nl_edge 4 -b 6 -clip 5 \
-        -p 100 -hidden_dim 512 -pooling_dim 4096 -lr 1e-3 -ngpu 1 -test -ckpt checkpoints/motifnet-sgcls-baserels/vgrel-19.tar -nepoch 50 -use_bias -cache motifnet_sgcls
+        -p 100 -hidden_dim 512 -pooling_dim 4096 -lr 1e-3 -ngpu 1 -test -ckpt checkpoints/$REEF_CHECKPOINT/$CHECKPOINT_FN -nepoch 50 -use_bias
     python models/eval_rels.py -m predcls -model motifnet -order leftright -nl_obj 2 -nl_edge 4 -b 6 -clip 5 \
-        -p 100 -hidden_dim 512 -pooling_dim 4096 -lr 1e-3 -ngpu 1 -test -ckpt checkpoints/motifnet-sgcls-baserels/vgrel-19.tar -nepoch 50 -use_bias -cache motifnet_predcls
+        -p 100 -hidden_dim 512 -pooling_dim 4096 -lr 1e-3 -ngpu 1 -test -ckpt checkpoints/$REEF_CHECKPOINT/$CHECKPOINT_FN -nepoch 50 -use_bias
 fi
 
 
