c8bde3401f2a44ffaaca88bf1c7219b343938549
Vincent Chen <vincentschen@yahoo.com>
2018-05-11 16:49:32 -0700

    simplify scripts for launching/eval models

--- scripts/eval_models_sgcls.sh
+++ scripts/eval_models_sgcls.sh
@@ -5,23 +5,18 @@ export CUDA_VISIBLE_DEVICES=$2
 
 if [ $1 == "0" ]; then
     echo "EVALING THE BASELINE"
+    CHECKPOINT_DIR=0511-base
+    CHECKPOINT_FILE=vgrel-19.tar
 
-    BASELINE_CHECKPOINT=motifnet-sgcls-baserels
-    CHECKPOINT_FN=vgrel-12.tar
-    python models/eval_rels.py -m sgcls -model motifnet -order leftright -nl_obj 2 -nl_edge 4 -b 6 -clip 5 \
-        -p 100 -hidden_dim 512 -pooling_dim 4096 -lr 1e-3 -ngpu 1 -test -ckpt checkpoints/$BASELINE_CHECKPOINT/$CHECKPOINT_FN -nepoch 50 -use_bias
-    python models/eval_rels.py -m predcls -model motifnet -order leftright -nl_obj 2 -nl_edge 4 -b 6 -clip 5 \
-        -p 100 -hidden_dim 512 -pooling_dim 4096 -lr 1e-3 -ngpu 1 -test -ckpt checkpoints/$BASELINE_CHECKPOINT/$CHECKPOINT_FN -nepoch 50 -use_bias
 elif [ $1 == "1" ]; then
-
-    REEF_CHECKPOINT=motifnet-sgcls-reefrels
-    CHECKPOINT_FN=vgrel-12.tar
     echo "EVALING MOTIFNET"
-    python models/eval_rels.py -m sgcls -model motifnet -order leftright -nl_obj 2 -nl_edge 4 -b 6 -clip 5 \
-        -p 100 -hidden_dim 512 -pooling_dim 4096 -lr 1e-3 -ngpu 1 -test -ckpt checkpoints/$REEF_CHECKPOINT/$CHECKPOINT_FN -nepoch 50 -use_bias
-    python models/eval_rels.py -m predcls -model motifnet -order leftright -nl_obj 2 -nl_edge 4 -b 6 -clip 5 \
-        -p 100 -hidden_dim 512 -pooling_dim 4096 -lr 1e-3 -ngpu 1 -test -ckpt checkpoints/$REEF_CHECKPOINT/$CHECKPOINT_FN -nepoch 50 -use_bias
+    CHECKPOINT_DIR=0511-reef0.99
+    CHECKPOINT_FILE=vgrel-14.tar
 fi
 
+python models/eval_rels.py -m sgcls -model motifnet -order leftright -nl_obj 2 -nl_edge 4 -b 6 -clip 5 \
+        -p 100 -hidden_dim 512 -pooling_dim 4096 -lr 1e-3 -ngpu 1 -test -ckpt checkpoints/$CHECKPOINT_DIR/$CHECKPOINT_FILE -nepoch 50 -use_bias
+python models/eval_rels.py -m predcls -model motifnet -order leftright -nl_obj 2 -nl_edge 4 -b 6 -clip 5 \
+        -p 100 -hidden_dim 512 -pooling_dim 4096 -lr 1e-3 -ngpu 1 -test -ckpt checkpoints/$CHECKPOINT_DIR/$CHECKPOINT_FILE -nepoch 50 -use_bias
 
 
--- scripts/train_models_sgcls.sh
+++ scripts/train_models_sgcls.sh
@@ -5,34 +5,25 @@ export CUDA_VISIBLE_DEVICES=$2
 
 if [ $1 == "0" ]; then
     echo "TRAINING BASELINE"
-
-    BASELINE_CHECKPOINT=motifnet-sgcls-baserels
-    VG_SGG_FN='10R-baseline-VG-SGG.h5'
-    VG_SGG_DICT_FN='10R-baseline-VG-SGG-dicts.json'
-    #HACK: replace lines in the config file with dataset names
-    sed -i "s/VG_SGG_FN.*/VG_SGG_FN = stanford_path('$VG_SGG_FN')/" config.py
-    sed -i "s/VG_SGG_DICT_FN.*/VG_SGG_DICT_FN = stanford_path('$VG_SGG_DICT_FN')/" config.py
-    python models/train_rels.py \
-        -m sgcls -model motifnet -order leftright -nl_obj 2 -nl_edge 4 -b 6 -clip 5 \
-        -p 100 -hidden_dim 512 -pooling_dim 4096 -lr 1e-3 \
-        -ngpu 1 \
-        -ckpt checkpoints/vgdet/vg-24.tar \
-        -save_dir checkpoints/$BASELINE_CHECKPOINT -nepoch 50 -use_bias
+    CHECKPOINT_NAME=0511-base
+    DATA_PREFIX=9R-baseline
 
 elif [ $1 == "1" ]; then
     echo "TRAINING MOTIFNET"
-
-    REEF_CHECKPOINT=motifnet-sgcls-reefrels
-    VG_SGG_FN='10R-reef-VG-SGG.h5'
-    VG_SGG_DICT_FN='10R-reef-VG-SGG-dicts.json'
-    #HACK: replace lines in the config file with dataset names
-    sed -i "s/VG_SGG_FN.*/VG_SGG_FN = stanford_path('$VG_SGG_FN')/" config.py
-    sed -i "s/VG_SGG_DICT_FN.*/VG_SGG_DICT_FN = stanford_path('$VG_SGG_DICT_FN')/" config.py
-    python models/train_rels.py \
-        -m sgcls -model motifnet -order leftright -nl_obj 2 -nl_edge 4 -b 6 -clip 5 \
-        -p 100 -hidden_dim 512 -pooling_dim 4096 -lr 1e-3 \
-        -ngpu 1 \
-        -ckpt checkpoints/vgdet/vg-24.tar \
-        -save_dir checkpoints/$REEF_CHECKPOINT -nepoch 50 -use_bias
+    CHECKPOINT_NAME=0511-reef0.99
+    DATA_PREFIX=9R-reef0.99
 fi
+
+VG_SGG_FN=${DATA_PREFIX}-VG-SG.h5
+VG_SGG_DICT_FN=${DATA_PREFIX}-VG-SG-dicts.json
+#HACK: replace lines in the config file with dataset names
+sed -i "s/VG_SGG_FN.*/VG_SGG_FN = stanford_path('$VG_SGG_FN')/" config.py
+sed -i "s/VG_SGG_DICT_FN.*/VG_SGG_DICT_FN = stanford_path('$VG_SGG_DICT_FN')/" config.py
+python models/train_rels.py \
+    -m sgcls -model motifnet -order leftright -nl_obj 2 -nl_edge 4 -b 6 -clip 5 \
+    -p 100 -hidden_dim 512 -pooling_dim 4096 -lr 1e-3 \
+    -ngpu 1 \
+    -ckpt checkpoints/vgdet/vg-24.tar \
+    -save_dir checkpoints/$CHECKPOINT_NAME \
+    -nepoch 50 -use_bias
 
