e385efa5ca8305e1016be0cde282fa3f46de6e45
Vincent Chen <vincentschen@yahoo.com>
2018-05-11 22:57:01 -0700

    add hp search script

---
+++ scripts/hp_search.py
@@ -0,0 +1,36 @@
+import argparse
+import random
+import os
+import subprocess
+import numpy as np
+from os.path import basename, splitext
+
+parser = argparse.ArgumentParser(description='Run the model with varying parameters.')
+parser.add_argument('--gpu', type=str, default='0',
+                    help='Specifies which GPU to use.')
+parser.add_argument('--model-prefix', type=str, default='sgcls',
+                    help='Specifies model prefix')
+parser.add_argument('--data-prefix', type=str, default='', 
+                    help='prefix for data we are using')
+parser.add_argument('--nruns', type=int, default=20,
+                    help='How many variations should we run?')
+args = parser.parse_args()
+
+for _ in range(args.nruns):
+    # Specify which parameters you want to sweep across and what function to use to sweep over them.
+    # For example, random.uniform will sweep over a range independently.
+    params = {
+        'lr': '%.8f' % 10 ** random.uniform(-6, -1),
+    }
+
+    model_name = os.path.join(args.prefix + '_' + '_'.join(
+            [k + ':' + str(splitext(basename(params[k]))[0]) if 'data' in k
+            else k + str(params[k]) for k in params])) 
+    arguments = ' '.join(['-' + k + ' ' + str(params[k]) for k in params])
+    train = 'CUDA_VISIBLE_DEVICES=' + args.gpu + ' python models/train_rels.py -m sgcls -model motifnet -order leftright -nl_obj 2 -nl_edge 4 -b 6 -clip 5 -p 100 -hidden_dim 512 -pooling_dim 4096 -ngpu 1 -ckpt checkpoints/vgdet/vg-24.tar -nepoch 50 -use_bias' \
+         + ' ' + arguments + ' ' \
+         + '-save_dir checkpoints/' + model_name \
+         + ' |& tee checkpoints/' + model_name + '/train.log'
+    print ('^'*89)
+    print (train)
+    #subprocess.call(train, shell=True)
