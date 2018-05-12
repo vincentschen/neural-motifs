86e53385ae2b5f4f092803310aaf69db8ab969fe
Vincent Chen <vincentschen@yahoo.com>
2018-05-11 20:50:26 -0700

    time training and save train logs to file in checkpoint

--- scripts/train_models_sgcls.sh
+++ scripts/train_models_sgcls.sh
@@ -10,8 +10,8 @@ if [ $1 == "0" ]; then
 
 elif [ $1 == "1" ]; then
     echo "TRAINING MOTIFNET"
-    CHECKPOINT_NAME=0511-reef0.99
-    DATA_PREFIX=9R-reef0.99
+    CHECKPOINT_NAME=0511-reef0.95-test
+    DATA_PREFIX=9R-reef0.95
 fi
 
 VG_SGG_FN=${DATA_PREFIX}-VG-SG.h5
@@ -19,11 +19,13 @@ VG_SGG_DICT_FN=${DATA_PREFIX}-VG-SG-dicts.json
 #HACK: replace lines in the config file with dataset names
 sed -i "s/VG_SGG_FN.*/VG_SGG_FN = stanford_path('$VG_SGG_FN')/" config.py
 sed -i "s/VG_SGG_DICT_FN.*/VG_SGG_DICT_FN = stanford_path('$VG_SGG_DICT_FN')/" config.py
-python models/train_rels.py \
+touch checkpoints/${CHECKPOINT_NAME}/train.log
+time python models/train_rels.py \
     -m sgcls -model motifnet -order leftright -nl_obj 2 -nl_edge 4 -b 6 -clip 5 \
     -p 100 -hidden_dim 512 -pooling_dim 4096 -lr 1e-3 \
     -ngpu 1 \
     -ckpt checkpoints/vgdet/vg-24.tar \
     -save_dir checkpoints/$CHECKPOINT_NAME \
-    -nepoch 50 -use_bias
+    -nepoch 50 -use_bias \
+    |& tee checkpoints/${CHECKPOINT_NAME}/train.log
 
